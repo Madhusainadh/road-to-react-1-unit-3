095ca1c92e142cca923a4f656bdb642e4cb8520b
madhusainadh <lalammadhusainadh150@gmail.com>
2022-06-12 11:44:32 +0530

    road to react 1 unit 3

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+function navbar(){
+    return `<h3><a href="./ele.html">ele</a></h3>
+    <h3><a href="./index.html">home</a></h3>
+    <h3><a href="./jel.html">jel</a></h3>
+    <h3><a href="#">login</a></h3>
+    <h3><a href="#">signup</a></h3>
+    `;
+}
+
+
+export default navbar;
